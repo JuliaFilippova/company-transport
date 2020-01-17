ff12f59a1c4d6b56fe92d2777bfbf1ba5aa13ce8
Julia Filippova <mega.yulia1996@mail.ru>
2020-01-17 15:15:42 +0300

    правка дизайна

--- js/main.js
+++ js/main.js
@@ -1,37 +1,72 @@
-
-var slides = document.querySelectorAll('#slides .slide');
+// слайдер автоматический
+// с помощью querySelectorAll получаем все слайды из контейнера
+var slide = document.querySelectorAll('#slides .slide');
 var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,3500);
+// задаем интервал в 3.5 секунды для следующего слайда (3500ms)
+var slideInterval = setInterval(nextSlide,3000);
  
+ //меняем класс для текущего слайда, чтобы спрятать его. 
+ // Свойство transition автоматически обрабатывает плавное затухание
 function nextSlide() {
-    slides[currentSlide].className = 'slide';
-    currentSlide = (currentSlide+1)%slides.length;
-    slides[currentSlide].className = 'slide showing';
+    slide[currentSlide].className = 'slide';
+    // Потом добавляем класс к текущему слайду. 
+    // Мы используем оператор % на случай, если это был последний слайд, чтобы вернуться к первому. 
+    // Данный оператор отлично подходит в случаях, когда необходимо выполнять математические операции с циклами типа часов или календаря.
+    // В нашем случае 5 слайдов. Посчитаем все числа: 1%5=1, 2%5=2, 3%5=3, 4%5=4, and 5%5=0.
+    // После получения индекса слайда мы меняем класс и показываем новый. И опять прозрачность обрабатывается свойством transition.
+    currentSlide = (currentSlide+1)%slide.length;
+    slide[currentSlide].className = 'slide showing';
 }
 
-// $(function () {
-// //Яндекс карта 
-// 	if($('#location').length) { 
-// 		ymaps.ready(init);
-// 	    function init(){
-// 	        var myMap = new ymaps.Map("map", {
-// 	            center: [55.80336172662323,37.586239589965786],
-// 	            zoom: 16,
-// 	            controls: [],
-// 	            behaviors: ['drag']
-// 	        });
-// 	        //яндекс метка
-// 	        var myPlacemark = new ymaps.Placemark(
-// 	        //координаты метки
-// 	        [55.80277556894847,37.58375049999996], {
-// 	        	ballonContent: 'Бутырская улица, 62',
-// 	        	iconCaption: 'Бутырская улица, 62'
-// 	        });
-// 	        //Добавление метки на карту
-// 	        myMap.geoObjects.add(myPlacemark);
-// 	        //Смещение центра карты
-// 	        var position = myMap.getGlobalPixelCenter();
-// 	        myMap.setGlobalPixelCenter([position[0] + 200, position[1]]);
-// 	    };
-// 	};
-// });
+
+
+// слайдер по клику
+let dots = document.getElementsByClassName("dot");
+	dotsArea = document.getElementsByClassName("dots-box")[0];
+	slides = document.getElementsByClassName("slides__item");
+	prevBtn = document.getElementsByClassName("btn-prev")
+	nextBtn = document.getElementsByClassName("btn-next")
+	slideIndex = 1;
+
+showSlides(slideIndex);
+
+
+
+function showSlides(n) {
+
+	if (n < 1 ) {
+		slideIndex = slides.length;
+	}else if (n > slides.length) {
+		slideIndex = 1
+	}
+	
+	for (let i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (let i = 0; i < dots.length; i++) {
+		dots[i].classList.remove("active");
+	}
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].classList.add("active");
+}
+
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+function currentSlide (n) {
+	showSlides(slideIndex = n)
+}
+prevBtn.onclick = function () {
+	plusSlides(-1)
+}
+nextBtn.onclick = function () {
+	plusSlides(1)
+}
+dotsArea.onclick = function(e) {
+	for (let i = 0; i < dots.length + 1; i++) {
+		if (e.target.classList.contains("dot") && e.target == dots[i - 1]) {
+			currentSlide(i);
+		}
+	}
+}
