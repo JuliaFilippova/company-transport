f5424f9772562dc271d468732fa179f4f12dc604
Julia Filippova <mega.yulia1996@mail.ru>
2020-01-17 15:44:06 +0300

    правка дизайна

--- js/main.js
+++ js/main.js
@@ -70,3 +70,22 @@ dotsArea.onclick = function(e) {
 		}
 	}
 }
+
+
+$(document).ready(function(){ 
+	// ПЛАВНЫЙ СКРОЛЛ
+    $(".head-menu").on("click","a", function (event) {
+
+    //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+
+    //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+    //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+})
